5f783113aec3c7868afbe4cd344a6911ec8cc1ea
Ane-Jouke Schat <ajschat@gmail.com>
2011-03-20 17:26:01 +0000

    0.57t RC2 finalize.

--- trunk/game/boe_local.h
+++ trunk/game/boe_local.h
@@ -80,7 +80,8 @@
 // 0.56			2nd Release (hotfix)			404(/408)	2/12/11		// Sent out to BETA group on 2/12/11
 // 0.57t		Alpha							424			2/26/11
 // 0.57t		Beta							428			3/2/11		// Sent out to BETA group on 3/2/11
-// 0.57t		Release Candidate				511			3/19/11		// Sent out to BETA group on 3/19/11
+// 0.57t		1st Release Candidate			511			3/19/11		// Sent out to BETA group on 3/19/11
+// 0.57t		2nd Release Candidate			516			3/20/11		// Sent out to BETA group on 3/20/11
 
 //===================================================================
 
