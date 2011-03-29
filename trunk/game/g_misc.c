4d5218d6dac615201106238e92bb0a69622392bb
Jordi Epema <jor_epema@hotmail.com>
2011-03-29 14:59:25 +0000

    Fixed spectator teleport shows effect.

--- trunk/game/g_misc.c
+++ trunk/game/g_misc.c
@@ -84,7 +84,7 @@ void TeleportPlayer ( gentity_t *player, vec3_t origin, vec3_t angles, qboolean 
 		trap_LinkEntity (player);
 	}
 	
-	if(!nojump)
+	if(!nojump && player->client->sess.team != TEAM_SPECTATOR)
 	G_PlayEffect ( G_EffectIndex("misc/electrical"),player->client->ps.origin, player->pos1);
 }
 
