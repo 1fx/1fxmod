90caeafb3b01514992a1bab1c79a28b7e20c5e02
Ane-Jouke Schat <ajschat@gmail.com>
2011-04-01 22:38:55 +0000

    Marking current progress as Alpha stage.

--- trunk/game/boe_local.h
+++ trunk/game/boe_local.h
@@ -12,7 +12,7 @@
 #endif
 #define INF_STRING "1fx. Mod"
 #define INF_VERSION_STRING_COLORED "^71fx^1. ^3Mod"
-#define INF_VERSION_DATE "3/27/11"
+#define INF_VERSION_DATE "4/2/11"
 #define TEST_VERSION "Developed by ^GBoe!Man ^7& ^6Henkie\n^1Running a Test version of the Mod\n\n"
 #define STABLE_VERSION "Developed by ^GBoe!Man ^7& ^6Henkie\n1fx.uk.to ^3| ^7i3D.net\n\n"
 
@@ -83,6 +83,7 @@
 // 0.57t		1st Release Candidate			511			3/19/11		// Sent out to BETA group on 3/19/11
 // 0.57t		2nd Release Candidate			521			3/21/11		// Sent out to BETA group on 3/21/11
 // 0.57			Release							525			3/27/11		// Sent out to BETA group on 3/27/11
+// 0.58t		Alpha							545			4/2/11
 
 //===================================================================
 
