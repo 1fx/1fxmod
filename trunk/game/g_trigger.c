9d0f9428e214bb4a66829fc5051bc7063d6f0712
Jordi Epema <jor_epema@hotmail.com>
2011-06-10 13:47:03 +0000

    Fixed issue where both side teleporters were spawned too high.

--- trunk/game/g_trigger.c
+++ trunk/game/g_trigger.c
@@ -747,6 +747,25 @@ void SP_teleporter(gentity_t* ent){
 	vec3_t		src;
 	vec3_t		origin1;
 
+	VectorSet( ent->r.mins, -ITEM_RADIUS, -ITEM_RADIUS, -ITEM_RADIUS );
+	VectorSet( ent->r.maxs, ITEM_RADIUS, ITEM_RADIUS, ITEM_RADIUS );
+	VectorSet( src, ent->origin_from[0], ent->origin_from[1], ent->origin_from[2] + 1 );
+	VectorSet( dest, ent->origin_from[0], ent->origin_from[1], ent->origin_from[2] - 4096 );
+	trap_Trace( &tr, src, ent->r.mins, ent->r.maxs, dest, ent->s.number, MASK_SOLID );
+
+	if ( tr.startsolid ) 
+	{
+		Com_Printf ("Teleporter: %s startsolid at %s\n", ent->classname, vtos(ent->origin_from));
+		G_FreeEntity( ent );
+		return;
+	}
+	ent->s.groundEntityNum = tr.entityNum;
+	VectorCopy(tr.endpos, ent->origin_from);
+	VectorCopy(tr.endpos, origin1);
+	origin1[2] += 30;
+	G_SetOrigin( ent,origin1 );
+	// origin_to until ground
+	
 	if(strstr(ent->both_sides, "yes")){
 		AddSpawnField("classname", "teleporter");
 		origin = va("%.0f %.0f %.0f", ent->origin_to[0], ent->origin_to[1], ent->origin_to[2]);
@@ -774,25 +793,7 @@ void SP_teleporter(gentity_t* ent){
 		"min_players" "0"
 		*/
 	}
-	VectorSet( ent->r.mins, -ITEM_RADIUS, -ITEM_RADIUS, -ITEM_RADIUS );
-	VectorSet( ent->r.maxs, ITEM_RADIUS, ITEM_RADIUS, ITEM_RADIUS );
-	VectorSet( src, ent->origin_from[0], ent->origin_from[1], ent->origin_from[2] + 1 );
-	VectorSet( dest, ent->origin_from[0], ent->origin_from[1], ent->origin_from[2] - 4096 );
-	trap_Trace( &tr, src, ent->r.mins, ent->r.maxs, dest, ent->s.number, MASK_SOLID );
 
-	if ( tr.startsolid ) 
-	{
-		Com_Printf ("Teleporter: %s startsolid at %s\n", ent->classname, vtos(ent->origin_from));
-		G_FreeEntity( ent );
-		return;
-	}
-	ent->s.groundEntityNum = tr.entityNum;
-	VectorCopy(tr.endpos, ent->origin_from);
-	VectorCopy(tr.endpos, origin1);
-	origin1[2] += 30;
-	G_SetOrigin( ent,origin1 );
-	// origin_to until ground
-	
 	//origin = va("%.0f %.0f %.0f", ent->r.currentOrigin[0], ent->r.currentOrigin[1], ent->r.currentOrigin[2]-30);
 	origin = va("%.0f %.0f %.0f", ent->r.currentOrigin[0], ent->r.currentOrigin[1], ent->r.currentOrigin[2]-30);
 	AddSpawnField("classname", "fx_play_effect");
