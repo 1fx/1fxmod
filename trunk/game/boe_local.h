98d475f5eb307f50670e7fe61a0b3ad25b75aa43
Ane-Jouke Schat <ajschat@gmail.com>
2012-04-21 06:02:58 +0000

    Date fix, so second finalize of 1fx. Mod 0.60t-alpha1.

--- trunk/game/boe_local.h
+++ trunk/game/boe_local.h
@@ -24,7 +24,7 @@
 
 // Boe!Man 7/12/11: Mod date.
 #ifdef Q3_VM
-#define INF_VERSION_DATE "1/7/12"
+#define INF_VERSION_DATE "4/21/12"
 #else
 #define YEAR ((((__DATE__ [9] - '0')) * 10 + (__DATE__ [10] - '0')))
 
@@ -127,7 +127,7 @@
 // 0.59t		2nd Beta						710			7/15/11		// Sent out to BETA group on 7/15/11
 // 0.59t		Release Candidate				749			9/20/11		// Sent out to BETA group on 9/20/11
 // 0.59			Release							759			1/4/12		// Sent out to BETA group on 1/4/12, to public on 1/5/12
-// 0.60t		1st Alpha						770			4/21/12
+// 0.60t		1st Alpha						771			4/21/12
 
 
 //===================================================================
