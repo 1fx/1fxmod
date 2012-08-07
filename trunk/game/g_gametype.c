e6a54ece29ed404c4f10710dc3e0c5a7c537e777
Ane-Jouke Schat <ajschat@gmail.com>
2012-08-07 09:02:53 +0000

    Removed "Respawned due to being spawned with no weapons" debug message.

--- trunk/game/g_gametype.c
+++ trunk/game/g_gametype.c
@@ -999,7 +999,7 @@ void CheckGametype ( void )
 			if(ent->client->ps.weapon == WP_NONE && !G_IsClientDead(ent->client) && current_gametype.value != GT_HS && ent->client->sunRespawnTimer == 0 && ent->client->sess.team != TEAM_SPECTATOR){ // Boe!Man 6/14/11: Don't respawn them if they're waiting to be respawned with the sun condition.
 				trap_UnlinkEntity (ent);
 				ClientSpawn(ent);
-				trap_SendServerCommand( -1, va("print \"^3[Info] ^7%s has been respawned due to being spawned with no weapons.\n\"", ent->client->pers.netname));
+				//trap_SendServerCommand( -1, va("print \"^3[Info] ^7%s has been respawned due to being spawned with no weapons.\n\"", ent->client->pers.netname));
 			}
 
 			players[ent->client->sess.team] ++;
