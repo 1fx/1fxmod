b5806558f1bebb536f5fd9a20f8700c15872db8e
Ane-Jouke Schat <ajschat@gmail.com>
2013-07-18 20:29:28 +0000

    A small change to the previous improper skin fix (no message on connects).

--- trunk/game/g_client.c
+++ trunk/game/g_client.c
@@ -2322,7 +2322,13 @@ void ClientSpawn(gentity_t *ent)
 		ClientUserinfoChanged ( client->ps.clientNum );
 	}
 	if(current_gametype.value != GT_HS && current_gametype.value != GT_HZ){
-		if(!(level.gametypeTeam[client->sess.team] && client->pers.identity && client->pers.identity->mTeam) || (level.gametypeData->teams && client->sess.team != TEAM_SPECTATOR && !strstr(level.gametypeTeam[client->sess.team], client->pers.identity->mTeam))){ // this skin does not belong to this team so change their Identity
+		// Boe!Man 7/18/13: Fixed improper skin crashing a server in rare scenarios. No message is needed because they can't really help this (most of the time this happens on connecting).
+		if(!(level.gametypeTeam[client->sess.team] && client->pers.identity && client->pers.identity->mTeam)){
+			client->sess.extraIdChanges = 0;
+			ClientUserinfoChanged ( client->ps.clientNum );
+		}
+		
+		if(level.gametypeData->teams && client->sess.team != TEAM_SPECTATOR && !strstr(level.gametypeTeam[client->sess.team], client->pers.identity->mTeam)){ // this skin does not belong to this team so change their Identity
 			trap_SendServerCommand(ent->s.number, va("print \"^3[Info] ^7Your skin has been changed because it did not match your team.\n\"") );
 			client->sess.extraIdChanges = 0; // Boe!Man 8/29/11: Also reset the extraIdChanges because this will bug it.
 			ClientUserinfoChanged ( client->ps.clientNum );
