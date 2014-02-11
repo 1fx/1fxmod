6019168c74f68dcf139610788c257341a7aa5daa
Ane-Jouke Schat <ajschat@gmail.com>
2014-02-11 12:47:24 +0000

    1fx. Mod 0.73t - beta1 finalize.

--- trunk/game/boe_local.h
+++ trunk/game/boe_local.h
@@ -19,7 +19,7 @@
 */
 // Boe!Man 1/6/13: One string to rule them all.
 #ifdef _RELEASE	// Boe!Man 6/30/13: Major and minor version throughout a development cycle.
-#define INF_VERSION_STRING "0.73t - alpha1"
+#define INF_VERSION_STRING "0.73t - beta1"
 #elif _NIGHTLY	// Boe!Man 6/30/13: Debug release w/ symbols and other debugging stuff. Should never be released to the public.
 #define INF_VERSION_STRING "0.73t"
 #endif
@@ -174,6 +174,7 @@
 // 0.72t		2nd Beta						1145		12/4/13		// Sent out to BETA group on 12/4/13
 // 0.72t		3rd Beta						1150		12/18/13	// Sent out to BETA group on 12/18/13
 // 0.72			Release							1165		1/22/14		// Sent out to public on 1/22/14
+// 0.73t		1st Beta						1185		2/11/14		// Sent out to BETA group on 2/11/14
 
 
 //===================================================================
