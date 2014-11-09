a46ef93bb801cc7f569d1c9f978757d45c946907
AJSchat <ajschat@gmail.com>
2014-11-09 13:16:30 +0100

    Fixed crash in SMOGH92 grenade drop and added support for mp_extraClips in zombie weapon drops.

--- trunk/game/g_items.c
+++ trunk/game/g_items.c
@@ -161,6 +161,14 @@ int Pickup_Weapon (gentity_t *ent, gentity_t *other, qboolean* autoswitch )
 		// Henkie 26/01/10 -> Dupe fix
 		other->client->ps.weapon = WP_KNIFE;
 		other->client->ps.weaponstate = WEAPON_READY;
+	}else if (current_gametype.value == GT_HZ){
+		int ammoIndex;
+
+		// Boe!Man 11/9/14: H&Z is the only gametype where we immediately add all clips as defined in the weaponfile.
+		ammoIndex = weaponData[weaponNum].attack[ATTACK_NORMAL].ammoIndex;
+		other->client->ps.ammo[ammoIndex] += weaponData[weaponNum].attack[ATTACK_NORMAL].extraClips * weaponData[weaponNum].attack[ATTACK_NORMAL].clipSize;
+		other->client->ps.clip[ATTACK_NORMAL][weaponNum] = weaponData[weaponNum].attack[ATTACK_NORMAL].clipSize;
+		other->client->ps.firemode[weaponNum] = BG_FindFireMode(weaponNum, ATTACK_NORMAL, WP_FIREMODE_AUTO);
 	}
 
 	return g_weaponRespawn.integer;
--- trunk/game/henk_functions.c
+++ trunk/game/henk_functions.c
@@ -624,7 +624,7 @@ static int weaponGroups[6][3] =
 {//      weapon1                  weapon2               weapon3 (optional)
 	{ WP_M1911A1_PISTOL,			WP_USSOCOM_PISTOL,		0			, },   //group 1
 	{ WP_MICRO_UZI_SUBMACHINEGUN,	WP_M3A1_SUBMACHINEGUN,	0			, },   //group 2
-	{ WP_SMOHG92_GRENADE,			0,						0			, },   //group 3
+	{ WP_SMOHG92_GRENADE,			WP_SMOHG92_GRENADE,		0			, },   //group 3
 	{ WP_M4_ASSAULT_RIFLE,			WP_AK74_ASSAULT_RIFLE,	WP_MSG90A1	, },   //group 4
 	{ WP_USAS_12_SHOTGUN,			WP_M60_MACHINEGUN,		0			, },   //group 5
 	{ WP_MM1_GRENADE_LAUNCHER,		WP_RPG7_LAUNCHER,		0			, },   //group 6
