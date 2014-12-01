4d5d47430c5de82dcaf34087cf811355b1201958
AJSchat <ajschat@gmail.com>
2014-12-01 19:50:52 +0100

    Fixed knockback not always pushing back in the right direction.

--- trunk/game/g_missile.c
+++ trunk/game/g_missile.c
@@ -283,12 +283,9 @@ void Henk_PushArea( gentity_t *ent )
 			if(level.time >= tent->client->sess.lastpush && tent->client->sess.team == TEAM_BLUE){
 				VectorCopy(tent->client->ps.viewangles, fireAngs);
 				AngleVectors( fireAngs, dir, NULL, NULL );	
-				dir[0] *= -1.0;
-				dir[1] *= -1.0;
+				dir[0] *= -dir[0];
+				dir[1] *= -dir[1];
 				dir[2] = 0.0;
-				#ifdef _DEBUG
-				Com_Printf("test\n");
-				#endif
 				VectorNormalize ( dir );
 				G_ApplyKnockback ( tent, dir, knockback );
 				originstr = va("%.0f %.0f %.0f", tent->r.currentOrigin[0], tent->r.currentOrigin[1], tent->r.currentOrigin[2]+45);
