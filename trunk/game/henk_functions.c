44cf3f9fca2784378933e201f3b7ce14b6aa102e
AJSchat <ajschat@gmail.com>
2014-12-08 18:10:49 +0100

    Added L2A2 to the random drops. The SMOGH92 is now in a higher group and the M60 is in  a lower group. Also made the code future proof.

--- trunk/game/henk_functions.c
+++ trunk/game/henk_functions.c
@@ -621,19 +621,19 @@ static int weaponDropOdds[6][6] =
 };
 
 static int weaponGroups[6][3] =
-{//      weapon1                  weapon2               weapon3 (optional)
-	{ WP_M1911A1_PISTOL,			WP_USSOCOM_PISTOL,		0			, },   //group 1
-	{ WP_MICRO_UZI_SUBMACHINEGUN,	WP_M3A1_SUBMACHINEGUN,	0			, },   //group 2
-	{ WP_SMOHG92_GRENADE,			WP_SMOHG92_GRENADE,		0			, },   //group 3
-	{ WP_M4_ASSAULT_RIFLE,			WP_AK74_ASSAULT_RIFLE,	WP_MSG90A1	, },   //group 4
-	{ WP_USAS_12_SHOTGUN,			WP_M60_MACHINEGUN,		0			, },   //group 5
-	{ WP_MM1_GRENADE_LAUNCHER,		WP_RPG7_LAUNCHER,		0			, },   //group 6
+{//  weapon1			           weapon2 (optional)		weapon3 (optional)
+	{ WP_M1911A1_PISTOL,			WP_USSOCOM_PISTOL,		0				, },   //group 1
+	{ WP_MICRO_UZI_SUBMACHINEGUN,	WP_M3A1_SUBMACHINEGUN,	0				, },   //group 2
+	{ WP_MSG90A1,					WP_M60_MACHINEGUN,		0				, },   //group 3
+	{ WP_M4_ASSAULT_RIFLE,			WP_AK74_ASSAULT_RIFLE,	0				, },   //group 4
+	{ WP_USAS_12_SHOTGUN,			WP_SMOHG92_GRENADE,		0				, },   //group 5
+	{ WP_MM1_GRENADE_LAUNCHER,		WP_RPG7_LAUNCHER,		WP_L2A2_GRENADE	, },   //group 6
 };
 
 void DropRandom( gentity_t *ent, int zombies){
 	gentity_t	*dropped;
 	gitem_t *item;
-	int group, random, i, DropGroup, start;
+	int group, random = 0, i, DropGroup, start;
 	int Odds[8][1];
 
 	if(zombies >= 1 && zombies <= 2){
@@ -672,12 +672,14 @@ void DropRandom( gentity_t *ent, int zombies){
         return;
 	}
 	#endif
-	if(weaponGroups[group][2] == 0)
-		random = irand(0,1); // randomize between 2 weapons
-	else
-		random = irand(0, 2); // randomize between 3 weapons
 
-	//Com_Printf("Random: %i\n", random);
+	if (weaponGroups[group][1] != 0){
+		if (weaponGroups[group][2] == 0){
+			random = irand(0,1); // randomize between 2 weapons
+		}else{
+			random = irand(0, 2); // randomize between 3 weapons
+		}
+	}
 
 	item = BG_FindWeaponItem ((weapon_t)weaponGroups[group][random]);
 	item->addAllAmmo = qtrue;
