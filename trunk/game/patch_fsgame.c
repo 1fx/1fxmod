9fd31ee1982681ed2c8799b31d365d866e8d50a9
AJSchat <ajschat@gmail.com>
2015-04-29 21:52:16 +0200

    Fixed fs_game patch not working on MinGW (Windows).

--- trunk/game/patch_fsgame.c
+++ trunk/game/patch_fsgame.c
@@ -9,19 +9,19 @@
 
 // Local function definitions.
 #ifdef _WIN32
-void Patch_fsgame0					(void);
+void Patch_fsgame0	(void);
 #endif // _WIN32
-static void Patch_fsgame			(char *s, const char *key, const char *value);
+void Patch_fsgame	(char *s, const char *key, const char *value);
 
 /*
 ==================
-Patch_autoDownloadDetour
+Patch_fsgame
 
-The actual code of where we check what file the client tries to download.
+The actual code of where we change the fs_game CVAR to the appropriate value.
 ==================
 */
 
-static void Patch_fsgame(char *s, const char *key, const char *value)
+void Patch_fsgame(char *s, const char *key, const char *value)
 {
 	if (strcmp(key, "fs_game") == 0){
 		#ifdef _GOLD
@@ -45,11 +45,18 @@ The Linux detour skips this step (call directly to fsgame).
 
 #ifdef _WIN32
 #ifdef __GNUC__
-__asm__(".globl Patch_fsgame \n\t"
-	"_Patch_fsgame: \n"
-	"call _Patch_fsgame_frame \n"
+// Determine proper address.
+#ifdef _GOLD
+#define ADDRESS "$0x0044C7ED"
+#else
+#define ADDRESS "$0x0044B74D"
+#endif // _GOLD
 
-	"push $0x0046EF20 \n\t"
+__asm__(".globl Patch_fsgame0 \n\t"
+	"_Patch_fsgame0: \n"
+	"call _Patch_fsgame \n"
+
+	"push " ADDRESS " \n\t"
 	"ret\n"
 	);
 #elif _MSC_VER
