0b153db662e630f189e328884cf684849870dcdf
AJSchat <ajschat@gmail.com>
2015-12-27 20:47:22 +0100

    Changed DoS protection patch, traffic is now checked on IP basis, so flooding clients will only be blocked instead of everybody. This also fixes services like GameTracker and game server panels.

--- trunk/game/patch_dos.c
+++ trunk/game/patch_dos.c
@@ -1,34 +1,30 @@
-// Copyright (C) 2014 - Boe!Man, Henkie.
+// Copyright (C) 2015 - Boe!Man, Henkie.
 //
-// patch_dos.c - Q3 getstatus DoS patch.
+// patch_dos.c - Q3 connectionless packet DoS patch.
 
 //==================================================================
 
 #include "patch_local.h"
 
-typedef enum {
-    GETSTATUS,
-    GETINFO,
+typedef struct request_s {
+    int             numRequests;
+    unsigned int    ip;
+    #ifdef _DEBUG
+    qboolean        exceededMessage;
+    #endif // _DEBUG
+}request_t;
 
-    MAX_REQUEST
-} request_t;
+#define DOS_REQUESTS_IP     64  // Maximum clients allowed to make requests simultaniously.
+#define DOS_REQUESTS_SEC    5   // Maximum requests from one client per second.
 
 // Local variable definitions.
-#ifdef _DEBUG
-static const char       nameReq[MAX_REQUEST][10]    = { "getstatus", "getinfo" };
-#endif // _DEBUG
-
-static int              numRequests[MAX_REQUEST]    = { 0 };    // Requests made. Reset every second.
-static int              requestTimer                = 0;        // To keep track how many requests have been made in a period of time.
-static const int        requestsPerSecond           = 20;       // Number of allowed requests per second.
+static request_t        requestsPerIP[DOS_REQUESTS_IP]  = { 0 };    // Requests made per IP. Resets every second.
+static int              requestTimer                    = 0;        // To keep track how many requests have been made in a period of time.
+static qboolean         blockAllTraffic                 = qfalse;   // This is true if all clients slots are fully flooded.
 
 // Local function definitions.
-#ifdef _WIN32
-void        Patch_dosDetour0    (void);
-qboolean    Patch_dosDetour     (const char *message, char *from, char *request);
-#elif __linux__
-void        Patch_dosDetour     (const char *message, char *from, char *request);
-#endif // _WIN32
+void        Patch_dosDetour0    ( void );
+qboolean    Patch_dosDetour     ( const char *message, char *from, char *req );
 
 
 /*
@@ -39,66 +35,88 @@ The actual code of where we check if the request exceeded the number of allowed 
 ==================
 */
 
-#ifdef _WIN32
-qboolean Patch_dosDetour(const char *message, char *from, char *request)
-#elif defined(__linux__) || defined(MACOS_X)
-void Patch_dosDetour(const char *message, char *from, char *request)
-#endif // _WIN32
+qboolean Patch_dosDetour(const char *message, char *from, char *req)
 {
-    int req = -1;
-    #ifdef _DEBUG
-    int i;
-    #endif // _DEBUG
+    int         i, ipNum;
+    char        ip[MAX_IP];
+    request_t   *request = NULL;
 
-    if (strcmp(request, "getstatus") == 0)
-        req = GETSTATUS;
-    else if (strcmp(request, "getinfo") == 0)
-        req = GETINFO;
+    if(blockAllTraffic && level.time < requestTimer){
+        return qfalse;
+    }
 
-    if (level.time >= requestTimer){
+    if(level.time > requestTimer){
+        // Second has passed, we can reset our state.
         requestTimer = level.time + 1000;
+        blockAllTraffic = qfalse;
 
+        // Check if we can flush the client table.
+        memset(requestsPerIP, 0, sizeof(requestsPerIP));
+
+        // Safe to assume we can take the first slot.
+        request = &requestsPerIP[0];
+    }
+
+    // Get the IP number.
+    Q_strncpyz(ip, from, sizeof(ip));
+    if(ip[0] == 0|| strtok(ip, ":") == NULL || (ipNum = (G_IP2Integer(ip))) == 0){
+        // Invalid IP specified, don't allow this packet.
+        // We don't print a warning message unless in local debug mode,
+        // so this can never be abused on live servers (print spam).
+        #ifdef _NIGHTLY
+        Com_Printf("DoS protection: invalid IP: %s -> %u\n", ip, ipNum);
+        #endif // _NIGHTLY
+        return qfalse;
+    }
+
+    // Only find an empty slot when we didn't get a client number assigned yet.
+    // Most of the time only one or perhaps two requests are made every second.
+    // This check significantly speeds up the search for a free slot.
+    if(request == NULL){
+        // Find the client number.
+        for(i = 0; i < DOS_REQUESTS_IP; i++){
+            if(requestsPerIP[i].ip == ipNum){
+                request = &requestsPerIP[i];
+                break;
+            }
+        }
+
+        // Find an empty slot if we didn't encounter this IP yet.
+        if(request == NULL){
+            for(i = 0; i < DOS_REQUESTS_IP; i++){
+                if(requestsPerIP[i].ip == 0){
+                    request = &requestsPerIP[i];
+                    request->ip = ipNum;
+                    break;
+                }
+            }
+        }
+    }
+
+    // There is no free slot left or an existing one found.
+    // This means someone is flooding us, from different IPs..
+    if(request == NULL){
+        Com_Printf("DoS protection: no more free IP slots for incoming traffic for another %d msec.\n", requestTimer - level.time);
+
+        blockAllTraffic = qtrue;
+        return qfalse;
+    }
+
+    // Check if this client sent too many requests already.
+    if (request->numRequests > DOS_REQUESTS_SEC){
+        // Too many requests for IP.
         #ifdef _DEBUG
-        for (i = 0; i < MAX_REQUEST; i++){
-            if (numRequests[i] >= requestsPerSecond)
-                Com_Printf("DoS protection: Too many %s requests per second: %d (%d allowed)\n", nameReq[i], numRequests[i], requestsPerSecond);
+        if(!request->exceededMessage){
+            Com_Printf("DoS protection: Too many requests per second from IP: %s, blocking for %d msec.\n", ip, requestTimer - level.time);
+            request->exceededMessage = qtrue;
         }
         #endif // _DEBUG
 
-        memset(numRequests, 0, sizeof(numRequests));
+        return qfalse;
     }
+    request->numRequests++;
 
-    if (req != -1){
-        numRequests[req]++;
-
-        if (numRequests[req] >= requestsPerSecond){
-            #ifdef __linux__
-            #ifdef _GOLD
-            __asm__("mov -0xc(%ebp),%ebx \n\t"
-                    "mov -0x8(%ebp),%esi \n\t"
-                    "mov -0x4(%ebp),%edi \n\t"
-                    "mov %ebp,%esp \n\t"
-                    "pop %ebp \n\t"
-                    "push $0x08058d26 \n\t"
-                    "ret \n");
-            #else
-            __asm__("lea 0xffffffd8(%ebp), %esp \n\t"
-                "pop %ebx \n\t"
-                "pop %esi \n\t"
-                "pop %edi \n\t"
-                "leave \n"
-                "push $0x08055263 \n\t"
-                "ret \n");
-            #endif // _GOLD
-            #elif _WIN32
-            return qfalse;
-            #endif // __linux__
-        }
-    }
-
-    #ifdef _WIN32
     return qtrue;
-    #endif // _WIN32
 }
 
 /*
@@ -109,38 +127,70 @@ The detour for both GCC and VC++.
 ==================
 */
 
-#ifdef _WIN32
 #ifdef __GNUC__
 // Determine proper address.
 #ifdef _GOLD
-#define ADDRESS  "$0x004793A5"
+#ifdef __linux__
+#define ADDRESS  "$0x08058b1f"
+#define ADDRESS2 "$0x08058d26"
+#elif _WIN32
+#define ADDRESS  "$0x004793a5"
 #define ADDRESS2 "$0x00479528"
+#endif // __linux__
 #else
+#ifdef __linux__
+#define ADDRESS  "$0x0805505c"
+#define ADDRESS2 "$0x08055263"
+#elif _WIN32
 #define ADDRESS  "$0x00476879"
-#define ADDRESS2 "$0x004769F5"
+#define ADDRESS2 "$0x004769f5"
+#endif // __linux__
 #endif // _GOLD
 
 __asm__(".globl Patch_dosDetour0 \n\t"
+    #ifdef _WIN32
     "_Patch_dosDetour0: \n"
     "call _Patch_dosDetour \n"
+    #elif __linux__
+    "Patch_dosDetour0: \n"
+    // Push the first parameter to the local stack frame.
+    // We retrieve it later when returning.
+    "movl 8(%ebp), %ecx \n\t"
+    "movl %ecx, -4(%ebp) \n\t"
+    "pop 8(%ebp) \n\t"
+    "call Patch_dosDetour \n"
+    #endif // _WIN32
 
     "cmp $0, %eax \n\t"
     "je notAllowed \n\t"
+    #ifdef __linux__
+    "movl -4(%ebp), %ecx \n\t"
+    "movl %ecx, 8(%ebp) \n\t"
+    "pop %ecx \n\t"
+    "pop -4(%ebp) \n\t"
+    #endif // __linux__
     "push " ADDRESS " \n\t"
     "ret \n"
 
     "notAllowed: \n\t"
+    #ifdef __linux__
+    "movl -4(%ebp), %ecx \n\t"
+    "movl %ecx, 8(%ebp) \n\t"
+    "pop %ecx \n\t"
+    "pop -4(%ebp) \n\t"
+    #endif // __linux__
     "push " ADDRESS2 " \n\t"
     "ret \n"
     );
+
 #elif _MSC_VER
 // Determine proper address.
 #ifdef _GOLD
-#define ADDRESS  0x004793A5
+#define ADDRESS  0x004793a5
 #define ADDRESS2 0x00479528
 #else
 #define ADDRESS  0x00476879
-#define ADDRESS2 0x004769F5
+#define ADDRESS2 0x004769f5
 #endif // _GOLD
 
 __declspec(naked) void Patch_dosDetour0()
@@ -161,7 +211,6 @@ __declspec(naked) void Patch_dosDetour0()
 
 #undef ADDRESS
 #undef ADDRESS2
-#endif // _WIN32
 
 /*
 ==================
@@ -175,17 +224,17 @@ void Patch_dosProtection()
 {
     #ifdef _WIN32
     #ifdef _GOLD
-    Patch_detourAddress("DoS protection", (long)&Patch_dosDetour0, 0x004793A5, qtrue);
+    Patch_detourAddress("DoS protection", (long)&Patch_dosDetour0, 0x004793a5, qtrue);
     #else
     Patch_detourAddress("DoS protection", (long)&Patch_dosDetour0, 0x00476879, qtrue);
     #endif // _GOLD
     #endif // _WIN32
-    
+
     #ifdef __linux__
     #ifdef _GOLD
-    Patch_detourAddress("DoS protection", (long)&Patch_dosDetour, 0x08058b1f, qfalse);
+    Patch_detourAddress("DoS protection", (long)&Patch_dosDetour0, 0x08058b1f, qfalse);
     #else
-    Patch_detourAddress("DoS protection", (long)&Patch_dosDetour, 0x0805505c, qfalse);
+    Patch_detourAddress("DoS protection", (long)&Patch_dosDetour0, 0x0805505c, qfalse);
     #endif // _GOLD
     #endif // __linux__
 }
