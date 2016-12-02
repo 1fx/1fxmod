1fx/1fxmod

commit c947906cbb86c0191cbc8f5ba6a1ff15b7eeddf3

Removed g_shortCommandStyle CVAR.
Added new "G_printInfoMessage" function to simplify Info message
printing.Added new "G_clientNumFromArg" function. Optimized some calls
to it.

trunk/game/boe_utils.c
@@ -2191,17 +2191,17 @@ char *Boe_parseCustomCommandArgs(gentity_t *ent, char *in, qboolean shortCmd)
                     arg2[0] = buf2[4]; // Copy the argument string to the new buffer, including a terminator.
                     arg2[1] = '\0';
                     if (!shortCmd || shortCmd && !argc){
-                        trap_Argv(henk_atoi(arg2) + 1, arg, sizeof(arg)); // Fetch arg.
+                        trap_Argv(atoi(arg2) + 1, arg, sizeof(arg)); // Fetch arg.
                     }else{
-                        if (argc && argc >= henk_atoi(arg2)) {
-                            Q_strncpyz(arg, G_GetChatArgument(henk_atoi(arg2)), sizeof(arg));
+                        if (argc && argc >= atoi(arg2)) {
+                            Q_strncpyz(arg, G_GetChatArgument(atoi(arg2)), sizeof(arg));
                         }else{
                             arg[0] = '\0';
                         }
                     }
 
                     #ifdef _DEBUG
-                    Com_Printf("Argument %i: %s\n", henk_atoi(arg2), arg);
+                    Com_Printf("Argument %d: %s\n", atoi(arg2), arg);
                     #endif
                     if (strlen(arg) == 0) {
                         trap_SendServerCommand(ent - g_entities, "print \"^3[Info] ^7Error: You need to append additional arguments to this command!\n\"");
@@ -2345,6 +2345,173 @@ char *G_GetChatArgument(int argNum)
     RemoveColorEscapeSequences(newArg);
 
     return newArg;
+}
+
+/*
+==============
+G_clientNumFromArg
+10/30/16 - 5:29 PM
+Gets client number from the argument.
+
+Has support for both regular Admin commands (console)
+and the short commands (! in chat).
+Can fail on request depending on supplied boolean parameters.
+==============
+*/
+
+int G_clientNumFromArg(gentity_t *ent, int argNum, const char *action,
+    qboolean aliveOnly, qboolean otherAdmins, qboolean shortCmd)
+{
+    char        arg[16];
+    char        cleanName[MAX_NETNAME];
+    int         argc, i, clientsFound;
+    int         clientID;
+    qboolean    name;
+
+    // Determine if there are parameters present.
+    // Not being present could indicate a short command from the console.
+    if (shortCmd) {
+        argc = G_GetChatArgumentCount();
+    }
+
+    // Now fetch the argument.
+    if (!shortCmd || shortCmd && !argc){
+        trap_Argv((shortCmd) ? argNum + 1 : argNum, arg, sizeof(arg));
+    }else{
+        Q_strncpyz(arg, G_GetChatArgument(1), sizeof(arg));
+    }
+
+    // Check if there's a parameter specified at all.
+    if(!strlen(arg)){
+        G_printInfoMessage(ent,
+            "You haven't entered a valid player ID/player name.");
+        return -1;
+    }
+
+    // We can continue.
+    // Now check whether the client specified an ID or a name.
+    name = qfalse;
+    for(i = 0; i < strlen(arg); i++){
+        if(!isdigit(arg[i])){
+            // Letter was found, safe to assume this is a name.
+            name = qtrue;
+            break;
+        }
+    }
+
+    if(name){ // Argument potentially contains a name.
+        char multipleNamesFound[512];
+
+        // Compare using lowercase characters.
+        Q_strlwr(arg);
+
+        // Loop through connected clients, and check who the client meant.
+        clientsFound = 0;
+        memset(multipleNamesFound, 0, sizeof(multipleNamesFound));
+        for(i = 0; i < level.numConnectedClients; i++){
+            gclient_t *cl = g_entities[level.sortedClients[i]].client;
+            Q_strncpyz(cleanName, cl->pers.cleanName, sizeof(cleanName));
+
+            if(strstr(Q_strlwr(cleanName), arg)){
+                // Match. Save the client ID.
+                clientID = i;
+                clientsFound++;
+
+                // Also fill the "multiple found" buffer.
+                // We might need to print this later on.
+                #ifdef __GNUC__
+                snprintf(multipleNamesFound + strlen(multipleNamesFound),
+                    sizeof(multipleNamesFound) - strlen(multipleNamesFound),
+                    "^1[#%i] ^7%s, ", clientID, cl->pers.cleanName);
+                #elif _MSC_VER_
+                _snprintf_s(multipleNamesFound + strlen(multipleNamesFound),
+                    sizeof(multipleNamesFound) - strlen(multipleNamesFound),
+                    "^1[#%i] ^7%s, ", clientID, cl->pers.cleanName);
+                #endif // __GNUC__
+            }
+        }
+
+        // Multiple clients found?
+        if(clientsFound > 1){
+            // Remove the trailing ", ".
+            multipleNamesFound[strlen(multipleNamesFound) -2] = '\0';
+
+            // Print message.
+            G_printInfoMessage(ent,
+                va("Multiple names found with ^3%s^7: %s",
+                arg, multipleNamesFound));
+
+            return -1;
+        }
+    }else{ // Argument only contains numbers.
+        clientID = atoi(arg);
+    }
+
+    // Do some checks, ID must be a valid ID.
+    if(clientID < 0 || clientID >= g_maxclients.integer){
+        G_printInfoMessage(ent,
+            "You haven't entered a valid player ID/player name.");
+        return -1;
+    }
+
+    if (g_entities[clientID].client->pers.connected == CON_DISCONNECTED){
+        G_printInfoMessage(ent, "This client is not connected.");
+        return -1;
+    }
+
+    // Check if the executing Admin is doing this,
+    // and not RCON. If so, perform some extra checks.
+    if(ent && ent->client
+        && ent->client->sess.admin && g_entities[clientID].client->sess.admin){
+        // Don't allow to execute the command on higher level Admins.
+        #ifdef _awesomeToAbuse
+        if(g_entities[clientID].client->sess.admin > ent->client->sess.admin
+            && !strstr(action, "forceteam") && !strstr(action, "!pm")
+            && ent->client->sess.dev != 2)
+        #else
+        if (g_entities[clientID].client->sess.admin > ent->client->sess.admin
+            && !strstr(action, "forceteam") && !strstr(action, "!pm"))
+        #endif // _awesomeToAbuse
+        {
+            G_printInfoMessage(ent,
+                va("You cannot %s higher level Admins.", action));
+            return -1;
+        }
+
+        // Don't allow this command on other Admins?
+        if(!otherAdmins){
+            G_printInfoMessage(ent,
+                "You cannot use this command on other Admins.");
+            return -1;
+        }
+    }
+
+    // Check if the targeted player has to be alive for this command.
+    if(aliveOnly){
+        // Are they dead?
+        #ifdef _3DServer
+        if(G_IsClientDead(g_entities[clientID].client)
+            && !g_entities[clientID].client->sess.deadMonkey)
+        #else
+        if(G_IsClientDead (g_entities[num].client))
+        #endif // _3DServer
+        {
+            G_printInfoMessage(ent,
+                va("You cannot %s dead players.", action));
+            return -1;
+        }
+
+        // Are they spectating?
+        if (G_IsClientSpectating(g_entities[clientID].client))
+        {
+            G_printInfoMessage(ent,
+                va("You cannot %s a spectator.", action));
+            return -1;
+        }
+    }
+
+    // Everything checked out, return the ID.
+    return clientID;
 }
 
 /*

trunk/game/boe_admcmds.c
@@ -294,236 +294,6 @@ qboolean Boe_removeClanMemberFromDb(gentity_t *adm, const char *value, qboolean 
 }
 
 /*
-====================
-Boe_ClientNumFromArg
-====================
-*/
-int Boe_ClientNumFromArg (gentity_t *ent, int argNum, const char* usage, const char* action, qboolean aliveOnly, qboolean otheradmins, qboolean shortCmd)
-{
-    char    arg[16] = "\0"; // increase buffer so we can process more commands
-    int     num = -1;
-    int i, y, x, count, r;
-    char numb[36], numbx[4];
-    qboolean first = qtrue;
-    qboolean space = qfalse;
-    int numberofclients = 0;
-    char string[1024] = "\0";
-    char string1[64] = "\0";
-    char cleanName[MAX_NETNAME];
-
-    trap_Argv( argNum, arg, sizeof( arg ) );
-    if(shortCmd){ // Henk 04/05/10 -> Handle the short admin commands.
-        num = -1;
-        for(i=0;i<16;i++){ // FIX ME HENK: could be out of bounds
-            if(arg[i] == ' '){
-                //trap_SendServerCommand( -1, va("print \"^3[Debug] ^7Arg[%i] = %s.\n\"", i, arg[i]));
-                if(henk_isdigit(arg[i+1]) && !henk_ischar(arg[i+2])){ // we check if the other 2 are chars(fixes !uc 1fx bug which uppercuts id 1)
-                num = atoi(va("%c%c", arg[i+1], arg[i+2]));
-                }else{
-                    for(x=0;x<=20;x++){ // FIX ME HENK: could be out of bounds -> Could be improved by starting at i+3(?)
-                        if(arg[x] == ' '){
-                            // Henk 08/09/10 -> Check for another arg in this.
-                            for(y=1;y<=sizeof(arg);y++){
-                                if(arg[x+y] == ' '){
-                                    space = qtrue;
-                                    break;
-                                }else{
-                                    if(first){
-                                        Com_sprintf(numb, sizeof(numb), "%c", arg[x+y]);
-                                        first = qfalse;
-                                    }else{
-                                        Com_sprintf(numb, sizeof(numb), "%s%c", numb, arg[x+y]);
-
-                                    }
-                                    //Com_Printf("%s\n", numb);
-                                }
-                            }
-                            if(space == qfalse){
-                                if(!henk_ischar(arg[x+1]) && !henk_ischar(arg[x+2])){
-                                    //trap_SendServerCommand( ent-g_entities, va("print \"^3[Info] ^7You haven't entered a valid player ID/player name.\n\"", arg, usage));
-                                    num = -1;
-                                    break;
-                                }
-                            // FIX ME HENK: could be out of bounds
-                                memset(numb, 0, sizeof(numb));
-                                for(r=x+1;r<strlen(arg);r++){
-                                    if(arg[r])
-                                        if(!numb[0])
-                                            Com_sprintf(numb, sizeof(numb), "%c", arg[r]);
-                                        else
-                                            Com_sprintf(numb, sizeof(numb), "%s%c", numb, arg[r]);
-                                    else
-                                        break;
-                                }
-                            //sprintf(numb, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", arg[x+1], arg[x+2], arg[x+3], arg[x+4], arg[x+5], arg[x+6], arg[x+7], arg[x+8], arg[x+9], arg[x+10], arg[x+11], arg[x+12], arg[x+13], arg[x+14], arg[x+15]);
-                            }
-                            break;
-                        }
-                    }
-                    memset(string, 0, sizeof(string));
-                    memset(string1, 0, sizeof(string1));
-                    numberofclients = 0;
-                    num = -1;
-                    //numb[strlen(numb)-1] = '\0';
-                    numb[strlen(numb)] = '\0';
-                    for(x=0;x<level.numConnectedClients;x++){
-                        //trap_SendServerCommand(-1, va("print\"^3[Debug] ^7%s comparing with %s.\n\"", g_entities[level.sortedClients[x]].client->pers.cleanName,numb));
-                        Q_strncpyz(cleanName, g_entities[level.sortedClients[x]].client->pers.cleanName, sizeof(cleanName));
-                        if(strstr(Q_strlwr(cleanName), Q_strlwr(numb))){
-                            //Com_Printf("%s\n", g_entities[level.sortedClients[x]].client->pers.cleanName);
-                            num = level.sortedClients[x];
-                            numberofclients += 1;
-                            Com_sprintf(string1, sizeof(string1), "^1[#%i] ^7%s, ",  num, g_entities[level.sortedClients[x]].client->pers.cleanName);
-                            Q_strncpyz(string+strlen(string), string1, strlen(string1)+1);
-                            //break;
-                        }
-                    }
-                    if(string[0] && string[1]){
-                        string[strlen(string) - 2] = '\0';
-                        if (numberofclients > 1){
-                            trap_SendServerCommand(ent->s.number, va("print\"^3[Info] ^7Multiple names found with ^3%s^7: %s\n\"", numb, string));
-                            return -1;
-                        }
-                    }
-                }
-                break;
-            }else if(henk_isdigit(arg[i]) && henk_ischar(arg[i-1]) && g_shortCommandStyle.integer){ // Henk 10/03/11 -> Add support for !u1/!r1 commands
-                count = 0;
-                for(x=i;x<=strlen(arg);x++){
-                    if(henk_isdigit(arg[x])){
-                        numbx[count] = arg[x];
-                        count += 1;
-                    }else{
-                        numbx[count] = '\0';
-                        break;
-                    }
-                }
-                num = atoi(numbx);
-                //trap_SendServerCommand(ent->s.number, va("print\"^3[Debug] ^7New short command style detected, Client number: %i.\n\"", num));
-            }
-        }
-        if(num == -1){ // Get second argument because they use it from the console
-            trap_Argv( 2, arg, sizeof( arg ) );
-            if(arg[0]){
-                if(henk_isdigit(arg[0])){
-                    num = atoi(arg);
-                }else{
-                    for(i=0;i<level.numConnectedClients;i++){
-                        //trap_SendServerCommand(-1, va("print\"^3[Debug] ^7%s comparing with %s.\n\"", g_entities[level.sortedClients[i]].client->pers.cleanName,numb));
-                        Q_strncpyz(cleanName, g_entities[level.sortedClients[i]].client->pers.cleanName, sizeof(cleanName));
-                        if(strstr(Q_strlwr(cleanName), Q_strlwr(arg))){
-                            num = level.sortedClients[i];
-                            break;
-                        }
-                        num = -1;
-                    }
-                }
-            }else{
-                num = -1;
-            }
-        }
-
-    }else{
-        if (arg[0] >= '0' && arg[0] <= '9')
-        {
-            num = atoi( arg );
-        }else if(arg[0]){
-            for(i=0;i<level.numConnectedClients;i++){
-                //trap_SendServerCommand(-1, va("print\"^3[Debug] ^7%s comparing with %s.\n\"", g_entities[level.sortedClients[i]].client->pers.cleanName,numb));
-                Q_strncpyz(cleanName, g_entities[level.sortedClients[i]].client->pers.cleanName, sizeof(cleanName));
-                if(strstr(Q_strlwr(cleanName), Q_strlwr(arg))){
-                    num = level.sortedClients[i];
-                break;
-                }
-            num = -1;
-            }
-        }
-    }
-
-    if ( num < 0 || num >= g_maxclients.integer )
-    {
-        if(ent && ent->client)
-        {
-            trap_SendServerCommand( ent-g_entities, va("print \"^3[Info] ^7You haven't entered a valid player ID/player name.\n\""));
-        }
-        else
-        {
-            Com_Printf("You haven't entered a valid player ID/player name.\n", num );
-        }
-        return -1;
-    }
-
-    if ( g_entities[num].client->pers.connected == CON_DISCONNECTED )
-    {
-        if(ent && ent->client)
-        {
-            trap_SendServerCommand( ent-g_entities, va("print \"^3[Info] ^7This client is not connected.\n\""));
-        }
-        else
-        {
-            Com_Printf("There is no client with the client number %d.\n", num);
-        }
-        return -1;
-    }
-
-    if(ent && ent->client)
-    {
-        if (g_entities[num].client->sess.admin ){
-            // Boe!Man 1/4/10: Higher level Admin fix.
-            #ifdef _awesomeToAbuse
-            if(g_entities[num].client->sess.admin > ent->client->sess.admin && !strstr(action, "forceteam") && !strstr(action, "!pm") && ent->client->sess.dev != 2){
-            #else
-            if (g_entities[num].client->sess.admin > ent->client->sess.admin && !strstr(action, "forceteam") && !strstr(action, "!pm")){
-            #endif // _awesomeToAbuse
-                trap_SendServerCommand( ent-g_entities, va("print \"^3[Info] ^7You cannot %s higher level Admins.\n\"", action));
-                return -1;
-            }
-        }
-    }
-    // Boe!Man 1/18/10: Using Admin commands on other Admins fix.
-    if (!otheradmins && g_entities[num].client->sess.admin){
-        if(ent && ent->client){
-            trap_SendServerCommand(ent->s.number, va("print\"^3[Info] ^7You cannot use this command on other Admins.\n\""));
-            return -1;
-        }
-    }
-
-    if(aliveOnly)
-    {
-        #ifdef _3DServer
-        if ( G_IsClientDead ( g_entities[num].client ) && !g_entities[num].client->sess.deadMonkey)
-        #else
-        if ( G_IsClientDead ( g_entities[num].client ) )
-        #endif // _3DServer
-        {
-            if(ent && ent->client)
-            {
-                trap_SendServerCommand( ent-g_entities, va("print \"^3[Info] ^7You cannot %s dead players.\n\"", action));
-            }
-            else
-            {
-                Com_Printf("You cannot %s dead Players.\n", action);
-            }
-            return -1;
-        }
-        if (G_IsClientSpectating(g_entities[num].client))
-        {
-            if(ent && ent->client)
-            {
-                trap_SendServerCommand( ent-g_entities, va("print \"^3[Info] ^7You cannot %s a spectator.\n\"", action));
-            }
-            else
-            {
-                Com_Printf("You cannot %s a Spectator.\n", action);
-            }
-            return - 1;
-        }
-    }
-
-    return num;
-}
-
-/*
 =============
 Boe_id
 Updated 1/28/11 - 11:35 AM
@@ -576,7 +346,7 @@ void Boe_freakOut(gentity_t *adm)
 {
     int idnum;
 
-    idnum = Boe_ClientNumFromArg(adm, 2, "", "", qtrue, qtrue, qfalse);
+    idnum = G_clientNumFromArg(adm, 2, "do this to", qtrue, qtrue, qfalse);
     if(idnum < 0)
         return;
 
@@ -676,7 +446,7 @@ qboolean Boe_dev_f ( gentity_t *ent )
         char *nameKey;
 
         // Print name as ASCII values.
-        idnum = Boe_ClientNumFromArg(ent, 2, "", "", qfalse, qtrue, qfalse);
+        idnum = G_clientNumFromArg(ent, 2, "do this to", qfalse, qtrue, qfalse);
         if (idnum < 0)
             return qtrue;
 

trunk/game/boe_local.h
@@ -272,8 +272,6 @@ on a Linux platform.
 
 // Boe!Man 3/30/10
 void    Boe_adm_f ( gentity_t *ent );
-int     Boe_ClientNumFromArg (gentity_t *ent, int argNum, const char* usage, const char* type, qboolean aliveOnly, qboolean otheradmins, qboolean shortCmd);
-
 
 //extern    admCmd_t AdminCommands[124];
 
@@ -583,9 +581,12 @@ void    Boe_checkRoof(gentity_t *ent);
 int Boe_firstDigitOfInt(int i);
 
 // Boe!Man 7/27/12: Function that replaces arguments in the actual CustomCommand action.
-char *Boe_parseCustomCommandArgs(gentity_t *ent, char *in, qboolean shortCmd);
-char *G_GetChatArgument(int argNum);
-int G_GetChatArgumentCount(void);
+char    *Boe_parseCustomCommandArgs(gentity_t *ent, char *in, qboolean shortCmd);
+
+// Boe!Man 10/30/16: Functions used for various chat-based command solutions.
+char    *G_GetChatArgument(int argNum);
+int     G_GetChatArgumentCount(void);
+int     G_clientNumFromArg(gentity_t *ent, int argNum, const char *action, qboolean aliveOnly, qboolean otherAdmins, qboolean shortCmd);
 
 // Boe!Man 7/29/12: CVAR that handles admins/clan members on subnet rather than Full IP -> This effectively disables the admin login system.
 extern  vmCvar_t    g_preferSubnets;

commit 05ec5d109ad965335346f6b0c94094c418b44edd

Fixed regression in commit c947906 for public release builds.

trunk/game/boe_utils.c
@@ -2472,7 +2472,7 @@ int G_clientNumFromArg(gentity_t *ent, int argNum, const char *action,
         if(G_IsClientDead(g_entities[clientID].client)
             && !g_entities[clientID].client->sess.deadMonkey)
         #else
-        if(G_IsClientDead (g_entities[num].client))
+        if(G_IsClientDead (g_entities[clientID].client))
         #endif // _3DServer
         {
             G_printInfoMessage(ent,